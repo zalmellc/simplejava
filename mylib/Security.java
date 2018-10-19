63a00e17e308198767271dbb2773acd8c2d369e5
Zachary Lewis <zacharylewis@Zacharys-MacBook-Air.local>
2018-10-19 10:03:46 -0500

    k

---
+++ mylib/Security.java
@@ -0,0 +1,15 @@
+package mylib;
+
+public class Security {
+
+public static String password;
+public static void setPassword(String data)
+{
+  password = data;
+}
+public static String getPrivateData()
+{
+   return password;
+}
+
+}
---
+++ program.java
@@ -0,0 +1,13 @@
+
+package demo;
+
+public class program {
+
+public static void main(String args[])
+{
+   String privatedata = mylib.Security.getPrivateData();
+
+   System.out.println(privatedata);
+}
+
+}
